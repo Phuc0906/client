a941d5e3d707ba96a158605512d63dd2e379c1fc
Phuc Hoang <phuchoang022002@gmail.com>
2023-11-17 11:08:40 +0700

    init UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import mammoth from "mammoth";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [file, setFile] = useState(null);
+    const [xmlContent, setXmlContent] = useState('');
+
+    const onFileInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        // @ts-ignore
+        console.log(e.target.files[0]);
+        // @ts-ignore
+        setFile(e.target.files[0]);
+
+    }
+
+    useEffect(() => {
+        console.log(xmlContent);
+    }, [xmlContent])
+
+    const onFileUploadHandle = () => {
+        console.log(file);
+
+        const formData = new FormData();
+        // @ts-ignore
+        formData.append("file", file);
+
+        axios.post('http://localhost:8080/api/file', formData).then(res => {
+            console.log(res);
+        }).catch(err => {
+            console.log(err);
+        })
+
+    }
+
+    return (
+        <div className="">
+            <div>
+                <label>Please insert file</label>
+
+                <input onChange={onFileInput} type={"file"}  />
+            </div>
+            <div>
+                <button onClick={onFileUploadHandle}>Upload</button>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
